5574b024761d52d87b23990d3339ee14f43d0939
Arvib Fazriansyah <85182275+arv-fazriansyah@users.noreply.github.com>
2023-10-08 01:22:08 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,17 +15,17 @@ then
 
     # Memeriksa arsitektur perangkat
     case `dpkg --print-architecture` in
-		aarch64)
-			archurl="arm64" ;;
-		arm)
-			archurl="armhf" ;;
-		amd64)
-			archurl="amd64" ;;
-		x86_64)
-			archurl="amd64" ;;
-		*)
-		echo "Arsitektur tidak dikenal"; exit 1;;
-	esac
+        aarch64)
+            archurl="arm64" ;;
+        arm)
+            archurl="armhf" ;;
+        amd64)
+            archurl="amd64" ;;
+        x86_64)
+            archurl="amd64" ;;
+        *)
+        echo -e "\e[31mArsitektur tidak dikenal\e[0m"; exit 1;;
+    esac
 
     # Mengunduh file-file yang diperlukan untuk berfungsi dengan baik
     git clone https://github.com/arv-fazriansyah/ubuntu-termux.git
@@ -40,7 +40,7 @@ then
 
     # Mengekstrak sistem ubuntu ke dalam folder yang telah dibuat di sistem
     sudo tar -xzf ./ubuntu-base.tar.gz --exclude='dev' -C $localbuild
-    
+
     # Mengubah izin file
     chmod 777 ./scripts/ubuntu
     chmod 644 ./scripts/passwd
@@ -65,8 +65,8 @@ then
     rm -rf ../ubuntu-termux
     rm ../install
 
-    echo "Instalasi berhasil!"
-    echo "Gunakan perintah 'ubuntu' untuk memulai sistem."
+    echo -e "\e[32mInstalasi berhasil!\e[0m"
+    echo -e "\e[32mGunakan perintah 'ubuntu' untuk memulai sistem.\e[0m"
 else
-    echo "Instalasi tidak berfungsi langsung dari root"
+    echo -e "\e[31mInstalasi tidak berfungsi langsung dari root\e[0m"
 fi
